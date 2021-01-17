14c9d01f2e6faf739116bd1132172292e07fb9ea
kraftdorian <75522596+kraftdorian@users.noreply.github.com>
2021-01-17 15:16:06 +0100

    Feature:- Styled app logo.

--- src/components/AppLogo.js
+++ src/components/AppLogo.js
@@ -1,5 +1,9 @@
+import "@/stylesheets/components/logo.scss";
+
 import logo from "@/assets/logo.svg";
 
-const AppLogo = () => <img className="header__logo" src={logo} alt="Logo" />;
+const AppLogo = () => (
+  <img className="app__logo" src={logo} alt="Logo" width={62} height={66} />
+);
 
 export default AppLogo;
