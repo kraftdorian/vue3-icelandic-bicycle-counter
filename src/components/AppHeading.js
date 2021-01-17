706e126f3b3bd973d2f865c74f6b561fd8042111
kraftdorian <work.kajzer.damian@gmail.com>
2021-01-17 15:08:42 +0100

    Feature:- Styled heading.

--- src/components/AppHeading.js
+++ src/components/AppHeading.js
@@ -1,6 +1,11 @@
+import "@/stylesheets/components/heading.scss";
+
 const AppHeading = ({ level }, context) => {
   const HeadingTag = `h${level}`;
-  return <HeadingTag>{context.slots.default()}</HeadingTag>;
+  const className = `app__heading app__heading--level-${level}`;
+  return (
+    <HeadingTag className={className}>{context.slots.default()}</HeadingTag>
+  );
 };
 
 export default AppHeading;
