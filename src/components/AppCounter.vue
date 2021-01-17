79b51a768bcbe70124eca4604bad9e81c1bd9fc4
kraftdorian <work.kajzer.damian@gmail.com>
2021-01-17 16:02:30 +0100

    Feature:- Created counter.

---
+++ src/components/AppCounter.vue
@@ -0,0 +1,20 @@
+<script>
+import AppSection from "@/components/ui/AppSection";
+import AppHeading from "@/components/ui/AppHeading";
+
+export default {
+  components: { AppSection, AppHeading },
+  setup() {
+    return {};
+  },
+  render() {
+    return (
+      <>
+        <AppSection>
+          <AppHeading level="2">Daily entries</AppHeading>
+        </AppSection>
+      </>
+    );
+  }
+};
+</script>
